e6dd3d9181a06342384ab7b9c9732af84a8ce856
Aman Kathait <aman.kathat011@gmail.com>
2025-04-04 13:19:30 +0530

    Left side bar with profile card completed

--- Frontend/hnbconnect_Frontend/src/components/Feed.jsx
+++ Frontend/hnbconnect_Frontend/src/components/Feed.jsx
@@ -6,7 +6,7 @@ export default function Feed() {
         <p className="text-gray-700">This is the first tweet in the feed.</p>
       </div>
       <div className="bg-white p-4 rounded-lg shadow mb-4">
-        <h2 className="font-bold">Tweet 2</h2>
+        <h2 className="font-bold">Tweet 3</h2>
         <p className="text-gray-700">This is the second tweet in the feed.</p>
       </div>
     </div>
